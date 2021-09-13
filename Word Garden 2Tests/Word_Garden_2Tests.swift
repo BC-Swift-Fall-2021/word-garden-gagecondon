3ce4f100ddec41bb16b3dded9fb7c9ae5a9250e0
Gage Condon <condongb@bc.edu>
2021-09-13 14:49:13 -0400

    Initial Commit

---
+++ Word Garden 2Tests/Word_Garden_2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Word_Garden_2Tests.swift
+//  Word Garden 2Tests
+//
+//  Created by Gage Condon on 9/13/21.
+//
+
+import XCTest
+@testable import Word_Garden_2
+
+class Word_Garden_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
